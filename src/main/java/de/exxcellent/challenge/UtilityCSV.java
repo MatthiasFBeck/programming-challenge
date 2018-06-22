714746ac70a9647c25cc6ede3a91c8e71ea8006d
Matthias Beck <MatthiasFabian.Beck@web.de>
2018-06-22 10:57:29 +0200

    Added loadCSV method to UtilityCSV. This will load and store the csv data in a List of Lists of Strings.

--- src/main/java/de/exxcellent/challenge/UtilityCSV.java
+++ src/main/java/de/exxcellent/challenge/UtilityCSV.java
@@ -11,7 +11,52 @@ public class UtilityCSV {
 	// storage for csv file content
 	List<List> data = new ArrayList<>();
 	
-	
+	// load csv file
+	public void loadCSV(String fileName){
+		
+		// empty data storage
+		data = new ArrayList<>();
+		
+		
+		BufferedReader br = null;
+		String line = "";
+		// define character data will be seperated by
+		String splittingChar = ",";
+		int lineCounter = 0;
+		
+		try {
+
+			br = new BufferedReader(new FileReader(fileName));
+			while ((line = br.readLine()) != null) {
+				
+				// add new ArrayList to data so next line of data can be stored
+				data.add(new ArrayList());
+				
+				// save line of data as lineInput
+				String[] lineInput = line.split(splittingChar);
+				
+				// fill data storage with current line of data
+				for (int j = 0; j < lineInput.length; j++){
+					data.get(lineCounter).add(lineInput[j]);
+				}
+				lineCounter++;
+
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 	
 
 }
