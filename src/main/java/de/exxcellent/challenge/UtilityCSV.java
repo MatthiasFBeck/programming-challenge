4a1d860d30ff2273aa3895a11669524be7b0a424
Matthias Beck <MatthiasFabian.Beck@web.de>
2018-06-22 11:57:22 +0200

    maxDist method added to UtilityCSV. method is an analog of minDist. Additionally added a constructor that takes a csv file name so there can't be an empty data object.

--- src/main/java/de/exxcellent/challenge/UtilityCSV.java
+++ src/main/java/de/exxcellent/challenge/UtilityCSV.java
@@ -1,10 +1,11 @@
 package de.exxcellent.challenge;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.*;
+import java.lang.IndexOutOfBoundsException;
 import java.lang.Math;
 import java.lang.Double;
 
@@ -12,6 +13,10 @@ public class UtilityCSV {
 	
 	// storage for csv file content
 	List<List> data = new ArrayList<>();
+	
+	public UtilityCSV(String fileName){
+		this.loadCSV(fileName);
+	}
 	
 	// load csv file
 	public void loadCSV(String fileName){
@@ -80,6 +85,29 @@ public class UtilityCSV {
 		}
 		return result;
 	}
+	
+	// Method takes 2 columns, calculates the line with the biggest absolute difference and returns the line name (first column entry of calculated line). Method expects a head: no data to be analyzed in first row.
+		public String maxDist(int col1, int col2){
+			String result = "";
+			
+				result = (String)data.get(1).get(0);
+				
+				// storage biggest absolute difference calculated so far. Initialize with difference of first row
+				Double biggestDiff = Math.abs(Double.parseDouble((String)data.get(1).get(col1)) - Double.parseDouble((String)data.get(1).get(col2)));
+				
+				for (int i = 1; i < data.size(); i++){
+					// store the two values of the current line
+					Double val1 = Double.parseDouble((String)data.get(i).get(col1));
+					Double val2 = Double.parseDouble((String)data.get(i).get(col2));
+					
+					if (Math.abs(val1 - val2) > biggestDiff){
+						biggestDiff = Math.abs(val1 - val2);
+						result = (String)data.get(i).get(0);
+					}
+				}
+			
+			return result;
+		}
 	
 
 }
